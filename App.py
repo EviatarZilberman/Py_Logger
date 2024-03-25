a0416332878d509466014b361a09e15f42004a8e
Eviatar Zilberman <esztbk@gmail.com>
2024-03-25 14:52:48 +0200

    Now logger is fine.

---
+++ App.py
@@ -0,0 +1,7 @@
+# from Models.Writer import Writer
+#
+# Writer(r'C:\Users\User\Projects\Logs', 'MyLogs.log').write_log('aaa')
+# writer.write_log('this is log!', 'e')
+#
+# writer = Writer(r'C:\Users\User\Projects\Logs', 'MyLogs111.log')
+# writer.write_log('this is new log!', 'e')
